00a68259bfdb0f880423942ced9f2f6a6c4a6ee8
Chris <chrismtz2003@gmail.com>
2026-07-06 03:59:45 -0400

    Added my Header for insurance_systems.py

---
+++ insurance_system.py
@@ -0,0 +1,9 @@
+"""
+Program Name: Insurance Quote Generator App
+Name: Chris Martinez
+File Purpose: Defines the profile object structure and the calculate_quote method
+Resources used: Python Crash Course (Chapters 8-11)
+Date: July 6, 2026
+"""
+
+
