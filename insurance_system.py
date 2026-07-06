0f836295f7bd28d43128c2a991a88e2e43c76918
Chris <chrismtz2003@gmail.com>
2026-07-06 04:09:00 -0400

    Establsihed my object __init__ and its attributes

--- insurance_system.py
+++ insurance_system.py
@@ -7,3 +7,17 @@ Date: July 6, 2026
 """
 
 
+class Profile:
+    """Here is the blueprint or class for my profile object"""
+    
+    def __init__(self, name, age, car_year, salary, driving_history, accidents):
+        """Here we havine the attributes"""
+        self.name = name
+        self.age = int(age)
+        self.car_year = int(car_year)
+        self.salary = salary
+        self.driving_history = driving_history
+        self.accidents = accidents
+        self.monthly_rate = 0.0
+
+        
