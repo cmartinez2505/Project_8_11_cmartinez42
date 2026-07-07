ad2787fa7b623b0bf1bbec7dd4a4db123ffbaa3b
Chris <chrismtz2003@gmail.com>
2026-07-07 03:00:31 -0400

    Added my header for data.py

---
+++ data.py
@@ -0,0 +1,8 @@
+"""
+Program Name: Insurance Quote Generator App
+Name: Chris Martinez
+File Purpose: Being able to read and save data into a JSON file
+Resources used: Python Crash Course (Chapters 8-11)
+Date: July 7, 2026
+"""
+
