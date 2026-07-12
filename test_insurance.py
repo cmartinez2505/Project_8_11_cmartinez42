43aed213d3d98699b83225661500fea248d3904e
Chris <chrismtz2003@gmail.com>
2026-07-12 16:58:57 -0400

    Added my header and my class to see if calculations work correct

---
+++ test_insurance.py
@@ -0,0 +1,13 @@
+"""
+Program Name: Insurance Quote Generator App
+Name: Chris Martinez
+File Purpose: Tests if our code works as it is supposed to.
+Resources used: Python Crash Course (Chapters 8-11)
+Date: July 12, 2026
+"""
+
+import unittest
+from insurance_system import QuoteCalculator
+
+class TestInsuranceCalculator(unittest.TestCase):
+    "Tests our calculations to see if they calculate correctly"
